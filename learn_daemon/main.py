fb21f122d8c0f8e6f7fed66d21359299082138c8
Christopher Mckerracher <ChrisLMckerracher@gmail.com>
2014-11-24 16:07:06 -0500

    Update main.py

--- learn_daemon/main.py
+++ learn_daemon/main.py
@@ -1,12 +1,12 @@
 import simple_daemon
 
-class ballsack(simple_daemon.simple_daemon):
+class test(simple_daemon.simple_daemon):
     #override simple_daemon's run
     def run(self):
         while True:
             pass
 
 
-ballclass = ballsack()
+testclass = test()
 
-ballclass.start()
+testclass.start()
